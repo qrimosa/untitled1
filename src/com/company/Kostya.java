fbe8342a260019c777a993b089b5ac4374373473
qrimosa <93031480+qrimosa@users.noreply.github.com>
2021-11-28 21:03:14 +0200

    Initial commit

---
+++ src/com/company/Kostya.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Kostya extends PersonInfo {
+    Kostya(int age, String name){
+        super(age, name);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Kostya k = new Kostya(20, "Kostya");
+        System.out.println(k.age + ", " + k.name);
+    }
+}
